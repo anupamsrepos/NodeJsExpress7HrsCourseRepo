ad6e10c06765929d0e7db20ac2e702d732bc126e
Anupam Nath <anupam1007@gmail.com>
2023-06-11 18:57:45 -0700

    Added register and authentication endpoints

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const {logger} = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
-const corsOptions = require('./config/corsOptions');
+const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 //custom middleware
@@ -14,32 +14,31 @@ app.use(logger);
 app.use(cors(corsOptions));
 
 //built-in middleware
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //server static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 //routes
-app.use('/', require('./routers/root'));
-app.use('/employees', require('./routers/api/employees'));
+app.use("/", require("./routers/root"));
+app.use("/register", require("./routers/api/register"));
+app.use("/auth", require("./routers/auth"));
+app.use("/employees", require("./routers/api/employees"));
 
 //This should be at the end of all request http verb
 //If none of the above HttpVerbs works then come here
-app.all('/*', (req, res) => {
-    res.status(404);
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    }
-    else if(req.accepts('json')){
-        res.json({error: "404 Not found"});
-    }
-    else{
-        res.type('txt').send('404 Not Found');
-    }    
-})
+app.all("/*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
